34424ffcfc2990f150ab975245e8ec2066f1d743
Brett Kirchdorfer <brettkirchdorfer@Mac.ax3000.online>
2025-04-02 06:27:04 -0400

    Add random tarot card generator

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
-import os
+import os, random
 
 app = Flask(__name__)
 
+tarot_cards = [
+    'The Fool', 'The Magician', 'The High Priestess', 'The Empress',
+    'The Emperor', 'The Hierophant', 'The Lovers', 'The Chariot',
+    'Strength', 'The Hermit', 'Wheel of Fortune', 'Justice',
+    'The Hanged Man', 'Death', 'Temperance', 'The Devil',
+    'The Tower', 'The Star', 'The Moon', 'The Sun',
+    'Judgement', 'The World'
+]
+
 @app.route('/')
-def hello():
-    return '🐸 Mystic Frog Oracle is LIVE!'
+def random_card():
+    card = random.choice(tarot_cards)
+    return f'🐸 Your Mystical Frog Oracle card is: {card}'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
